e9f61b3e26265dd140b9cd548ee42b89d47bdb28
dokidia <42838268+twk1024@users.noreply.github.com>
2020-07-19 02:01:09 +0900

    Update NoDamage.php

--- src/dokidia/NoDamage.php
+++ src/dokidia/NoDamage.php
@@ -3,3 +3,18 @@
 namespace dokidia;
 
 use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class NoDamage extends PluginBase implements Listener {
+
+      public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+  
+      public function NoDamage(EntityDamageEvent $damage){
+        
+       $damage->setCancelled(true);
+      }
+}
